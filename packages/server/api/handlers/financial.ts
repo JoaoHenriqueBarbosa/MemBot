a3b9fe0dbcc9bfdc851f9ba14431200153e7482f
JoaoHenriqueBarbosa <joaohenriquebarbosa21@gmail.com>
2024-06-24 13:05:26 -0300

    feat: Added new financial API endpoints for getting total income, total expense, and balance.

--- packages/server/api/handlers/financial.ts
+++ packages/server/api/handlers/financial.ts
@@ -1,4 +1,16 @@
 
-export const financialHandler = async (req: Request) => {
+export const getFinancial = async (req: Request) => {
     return new Response("Financial API");
 };
+
+export const getTotalIncome = async (req: Request) => {
+    return new Response("Total Income API");
+}
+
+export const getTotalExpense = async (req: Request) => {
+    return new Response("Total Expense API");
+}
+
+export const getBalance = async (req: Request) => {
+    return new Response("Balance API");
+}
