77681bc24b431734957fc03de411d340eb27e897
JoaoHenriqueBarbosa <joaohenriquebarbosa21@gmail.com>
2024-07-19 14:26:22 -0300

    chore: Update Vite config to include Vercel plugin for deployment

--- packages/public/vite.config.ts
+++ packages/public/vite.config.ts
@@ -1,9 +1,9 @@
 import path from "path"
-import react from "@vitejs/plugin-react-swc"
+import vercel from 'vite-plugin-vercel';
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [vercel()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
